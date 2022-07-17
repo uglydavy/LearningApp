64dbbb9b719ad213ac8ad283f162260134c6934d
uglydavy <aknk.v@protonmail.ch>
2022-07-17 22:16:28 +0800

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by 𝔲𝔤𝔩𝔶 ♡ on 7/17/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
